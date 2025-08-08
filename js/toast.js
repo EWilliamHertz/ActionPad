9a2600ddb30f271813c48794e27f5e6631f1dd5e
EWilliamHertz <74967501+EWilliamHertz@users.noreply.github.com>
2025-08-08 20:26:06 +0200

    Create toast.js

---
+++ js/toast.js
@@ -0,0 +1,15 @@
+const toastContainer = document.getElementById('toast-container');
+
+export function showToast(message, type = 'success') {
+    if (!toastContainer) return;
+
+    const toast = document.createElement('div');
+    toast.className = `toast ${type}`;
+    toast.textContent = message;
+
+    toastContainer.appendChild(toast);
+
+    setTimeout(() => {
+        toast.remove();
+    }, 4000);
+}
