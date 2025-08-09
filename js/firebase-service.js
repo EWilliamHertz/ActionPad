b9af776cf7f87191e1ea8f9a66f15d8bbc917d1b
EWilliamHertz <74967501+EWilliamHertz@users.noreply.github.com>
2025-08-09 09:53:04 +0200

    Update firebase-service.js

--- js/firebase-service.js
+++ js/firebase-service.js
@@ -11,6 +11,7 @@ import {
     query, where, serverTimestamp, setDoc, onSnapshot, orderBy
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 import { ref as storageRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
+import { showToast } from './toast.js';
 
 const usersCollection = collection(db, 'users');
 const companiesCollection = collection(db, 'companies');
