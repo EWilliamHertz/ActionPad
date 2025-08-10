2976d0aca96dc5edb0a9514ec66a43d58c8d5740
EWilliamHertz <74967501+EWilliamHertz@users.noreply.github.com>
2025-08-10 20:30:06 +0200

    Update user.js

--- js/services/user.js
+++ js/services/user.js
@@ -1,9 +1,13 @@
+// FILE: js/services/user.js
 import { doc, getDoc, updateDoc, collection } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { ref as storageRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 import { db, storage } from '../firebase-config.js';
 
-export const getUserProfile = (userId) => getDoc(doc(collection(db, 'users'), userId));
-export const updateUserProfile = (userId, newData) => updateDoc(doc(collection(db, 'users'), userId), newData);
+// Get a reference to the 'users' collection directly from the initialized db instance.
+const usersCollection = collection(db, 'users');
+
+export const getUserProfile = (userId) => getDoc(doc(usersCollection, userId));
+export const updateUserProfile = (userId, newData) => updateDoc(doc(usersCollection, userId), newData);
 
 export const uploadAvatar = async (userId, file) => {
     const filePath = `avatars/${userId}/${file.name}`;
@@ -11,4 +15,3 @@ export const uploadAvatar = async (userId, file) => {
     await uploadBytes(fileRef, file);
     return getDownloadURL(fileRef);
 };
-
